0d6fd4c5f582e50673f745594756e723bfd90ccc
moomin <tosh23fz@student.ju.se>
2026-05-11 13:12:10 +0200

    change switch to try and catch

--- TodoApi/Shared/ExceptionHandlers/GlobalExceptionHandler.cs
+++ TodoApi/Shared/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -22,26 +22,34 @@ public class GlobalExceptionHandler : IExceptionHandler
         HttpContext httpContext,
         Exception exception,
         CancellationToken cancellationToken)
-    {
-        var (statusCode, title, type) = exception switch
-        {
-            BadRequestException => (
-                StatusCodes.Status400BadRequest,
-                "Bad Request",
-                "https://tools.ietf.org/html/rfc9110#section-15.5.1"),
-            ConflictException => (
-                StatusCodes.Status409Conflict,
-                "Conflict",
-                "https://tools.ietf.org/html/rfc9110#section-15.5.10"),
-            NotFoundException => (
-                StatusCodes.Status404NotFound,
-                "Not Found",
-                "https://tools.ietf.org/html/rfc9110#section-15.5.5"),
-            _ => (
-                StatusCodes.Status500InternalServerError,
-                "Internal Server Error",
-                "https://tools.ietf.org/html/rfc9110#section-15.6.1"),
-        };
+    {   
+        int statusCode ;
+        string title ;
+        string type ;
+
+       try{
+            throw exception;
+       }
+       catch(BadRequestException ex){
+        statusCode = StatusCodes.Status400BadRequest;
+        title = "Bad Request";
+        type = "https://tools.ietf.org/html/rfc9110#section-15.5.1";
+       }
+       catch(ConflictException ex){
+        statusCode = StatusCodes.Status409Conflict;
+        title = "Conflict";
+        type = "https://tools.ietf.org/html/rfc9110#section-15.5.10";
+       }
+       catch(NotFoundException ex){
+        statusCode = StatusCodes.Status404NotFound;
+        title = "Not Found";
+        type = "https://tools.ietf.org/html/rfc9110#section-15.5.5";
+       }
+       catch(Exception ex){
+        statusCode = StatusCodes.Status500InternalServerError;
+        title = "Internal Server Error";
+        type = "https://tools.ietf.org/html/rfc9110#section-15.6.1";
+       }
 
         if (statusCode == StatusCodes.Status500InternalServerError)
             _logger.LogError(exception, "An unhandled exception occurred: {ExceptionMessage}", exception.Message);
